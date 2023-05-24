9d2c084001f766b8616830dfe8afc30dcb417ee9
nicolaazonta <nicolazonta114@gmail.com>
2023-05-24 16:16:01 +0200

    display correctly the cards in page

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        //dd($movies);
+        return view('home', compact('movies'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,18 @@
 
 @section('content')
     <div class="container w-75">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
-            <div class="col">
-                <div class="card">
-    
-                    ALOHA
-    
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
+
+            @forelse ($movies as $movie)
+                <div class="col">
+                    <div style="background-color: lightskyblue" class="card text-center p-1 h-100">
+
+                        <h2>{{ $movie->title }}</h2>
+
+                    </div>
                 </div>
-    
-            </div>
+            @empty
+            @endforelse
         </div>
     </div>
 @endsection
